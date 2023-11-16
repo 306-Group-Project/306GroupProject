04bb2a73d7e199f7ce3a0db8e5711f6660b604a7
Tanner L <tdroque@gmail.com>
2023-11-16 13:12:43 -0600

    shake windmill when taking damage

--- GroupProject306/Assets/Scripts/WindMill.cs
+++ GroupProject306/Assets/Scripts/WindMill.cs
@@ -7,11 +7,14 @@ public class WindMill : MonoBehaviour
     [SerializeField] public float health = 100.0f;
     public GameManager manager;
     public GameObject healthbar;
-    
+
+    private WaitForSeconds pause;
+    private bool isAnimating = false;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        pause = new WaitForSeconds(0.04f);
     }
 
     // Update is called once per frame
@@ -32,4 +35,71 @@ public class WindMill : MonoBehaviour
     {
         return health;
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isAnimating == false && other.gameObject.tag=="Enemy")
+        {
+            if (gameObject.transform.position.x < other.gameObject.transform.position.x)
+            {
+                StartCoroutine(ShakeRight());
+            }
+            else
+            {
+                StartCoroutine(ShakeLeft());
+            }
+        }
+    }
+
+    private IEnumerator ShakeLeft()
+    {
+        isAnimating = true;
+
+        for (int i = 0; i < 4; i++)
+        {
+            gameObject.transform.Rotate(0f, 0f, 2f);
+
+            yield return pause;
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            gameObject.transform.Rotate(0f, 0f, -2f);
+
+            yield return pause;
+        }
+
+        gameObject.transform.Rotate(0f, 0f, 2f);
+
+        yield return pause;
+
+        isAnimating = false;
+    }
+
+    private IEnumerator ShakeRight()
+    {
+        isAnimating = true;
+
+        for (int i = 0; i < 4; i++)
+        {
+            gameObject.transform.Rotate(0f, 0f, -2f);
+
+            yield return pause;
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            gameObject.transform.Rotate(0f, 0f, 2f);
+
+            yield return pause;
+        }
+
+        gameObject.transform.Rotate(0f, 0f, -2f);
+
+        yield return pause;
+
+        isAnimating = false;
+    }
+
+
 }
