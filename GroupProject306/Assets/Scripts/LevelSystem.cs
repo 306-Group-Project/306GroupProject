89de85a910f7e4842088949b1650b6b0ccaabf51
jal794 <jal794@usask.ca>
2023-12-08 11:38:15 -0600

    Added Sounds, Updated Screen, Level Count Fix

--- GroupProject306/Assets/Scripts/LevelSystem.cs
+++ GroupProject306/Assets/Scripts/LevelSystem.cs
@@ -30,7 +30,9 @@ public class LevelSystem : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        levelCount = 1;
         GenerateEnemies();
+        
     }
 
     // Update is called once per frame
